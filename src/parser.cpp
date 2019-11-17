a513514838d9655a2a0cfce2ea2548e145d28545
Paul <p.pietrzak166@gmail.com>
2019-11-17 20:40:33 +0100

    Import parsing update
    Not finished. Need to add code that adds nodes and clears appropriate variables and vectors

--- src/parser.cpp
+++ src/parser.cpp
@@ -32,17 +32,40 @@ std::string Parser::isImportPresent(std::string fileContent)
 void Parser::parseMIBImportFile(std::string fileContent)
 {
     std::smatch match;
-    std::regex pattern("\\n.*?OBJECT IDENTIFIER.*?}");
-    std::string nodeLines;
+    std::regex patternOIDLine(".*?OBJECT IDENTIFIER.*?}");
+    std::string nodeLine;
+    std::string nodeName;
+    std::vector<std::string> parentVector;
     //This will probably be the best way to parse everything for now
-    while (std::regex_search(fileContent, match, pattern))
+    while (std::regex_search(fileContent, match, patternOIDLine))
     {
-        nodeLines = match.str(0);
-        //parse arguments and add nodes here
-        std::regex_replace(fileContent, pattern, "");
-    }
-    
-    
+        std::string nodeLine = match.str(0);
+        //Here insert code to parse the rest
+        std::regex patternNodeName("(\\w*)\\s*OBJECT IDENTIFIER");
+        if (std::regex_search(nodeLine, match, patternNodeName))
+        {
+            nodeName = match.str(1);
+            std::regex patternCutNodeName(nodeName);
+            nodeLine = std::regex_replace(nodeLine, patternCutNodeName, "", std::regex_constants::format_first_only);
+        }
+        
+        //std::regex patternParentNames("\\{\\s*(\\S*).*?\\}");
+        std::regex patternParentNames("\\s+?([^ \\{]\\S*)\\s+?\\d\\s*\\}");
+        while (std::regex_search(nodeLine, match, patternParentNames))
+        {
+            parentVector.push_back(match.str(1));
+            
+            std::regex patternCutBraces("\\(");
+            parentVector.back() = std::regex_replace(parentVector.back(), patternCutBraces, "\\(", std::regex_constants::format_first_only);
+            patternCutBraces.assign("\\)");
+            parentVector.back() = std::regex_replace(parentVector.back(), patternCutBraces, "\\)", std::regex_constants::format_first_only);
+            
+            std::regex patternCut(parentVector.back());
+            nodeLine = std::regex_replace(nodeLine, patternCut, "", std::regex_constants::format_first_only);
+        } 
+        fileContent = std::regex_replace(fileContent, patternOIDLine, "", std::regex_constants::format_first_only);
+        
+    }   
 }
 
 void Parser::parseMIBFile(std::string fileContent)
