00baa3bdece5425b9f4dba06ee4fe97e3d2947e1
Paul <p.pietrzak166@gmail.com>
2019-11-29 15:19:15 +0100

    Import parsing upgraded

--- include/parser.hpp
+++ include/parser.hpp
@@ -18,6 +18,12 @@ class Parser
         void parseMIBFile(std::string fileContent);
         
     private:
+        std::string parseLineForNodeNameInImport(std::string & nodeLine, std::string & nodeName);
+        unsigned int parseLineForOIDInImport(std::string & nodeLine, unsigned int & OID);
+        void parseLineForParentNamesInImport(std::string & nodeLine, std::vector<std::string> & parentVector, std::vector<unsigned int> & vectorOID);
+        void addParentNodes(std::vector<std::string> & parentVector, std::vector<unsigned int> & vectorOID);
+        Node * addNodeFromImport(std::string & nodeName, unsigned int & OID, std::vector<std::string> & parentVector);
+        
         std::vector<std::string> m_importList;
         std::unique_ptr<Tree> m_pTree; 
         Node * m_pNode;
--- src/parser.cpp
+++ src/parser.cpp
@@ -32,99 +32,33 @@ std::string Parser::isImportPresent(std::string fileContent)
 
 void Parser::parseMIBImportFile(std::string fileContent)
 {
-    std::smatch match;
-    std::regex patternOIDLine(".*?OBJECT IDENTIFIER.*?}");
-    std::regex patternNodeName("(\\w*)\\s*OBJECT IDENTIFIER");
-    std::regex patternCutNodeName("");
-    std::regex patternOID("(\\d)\\s\\}");
-    std::regex patternParentNames("\\s+?([^ \\{]\\S*)\\s+?\\d\\s*\\}");
-    std::regex patternCutBraceLeft("\\(");
-    std::regex patternCutBraceRight("\\)");
-    std::regex patternParentOID("\\(.\\)");
-    std::vector<unsigned int> vectorOID;
-    std::vector<std::string> parentVector;
     std::string nodeLine;
     std::string nodeName;
     unsigned int OID;
-    //Parse for node string line
+    std::vector<std::string> parentVector;
+    std::vector<unsigned int> vectorOID;
+    std::regex patternOIDLine(".*?OBJECT IDENTIFIER.*?}");
+    std::smatch match;
+    
     while (std::regex_search(fileContent, match, patternOIDLine))
     {
         nodeLine = match.str(0);
-        //Parse for node name
-        if (std::regex_search(nodeLine, match, patternNodeName))
-        {
-            nodeName = match.str(1);
-            patternCutNodeName.assign(nodeName);
-            nodeLine = std::regex_replace(nodeLine, patternCutNodeName, "", std::regex_constants::format_first_only);
-        }
+        nodeName = parseLineForNodeNameInImport(nodeLine, nodeName);
+        OID = parseLineForOIDInImport(nodeLine, OID);
+        parseLineForParentNamesInImport(nodeLine, parentVector, vectorOID); 
         
-        //Parse for node OID
-        if (std::regex_search(nodeLine, match, patternOID))
-        {
-            OID = std::stoi(match.str(1));
-        }
-        
-        //Parse for parent names
-        while (std::regex_search(nodeLine, match, patternParentNames))
-        {
-            std::string matchString = match.str(1);
-            parentVector.push_back(matchString);
-            matchString = std::regex_replace(matchString, patternCutBraceLeft, "\\(", std::regex_constants::format_first_only);
-            matchString = std::regex_replace(matchString, patternCutBraceRight, "\\)", std::regex_constants::format_first_only);
-            std::regex patternCut(matchString);
-            nodeLine = std::regex_replace(nodeLine, patternCut, "", std::regex_constants::format_first_only);
-        } 
-        fileContent = std::regex_replace(fileContent, patternOIDLine, "", std::regex_constants::format_first_only);
-        //Parse parentVector for parents' OID (number in brackets)
         if (parentVector.size() != 1)
         {
-            for (std::vector<std::string>::size_type i = parentVector.size() - 1; i != (std::vector<std::string>::size_type) - 1 ; i--)
-            {
-                if (regex_search(parentVector[i], match, patternParentOID))
-                {
-                    std::string numberInBraces = match.str(0);
-                    numberInBraces.erase(0, 1);
-                    numberInBraces.erase(numberInBraces.size() - 1);
-                    vectorOID.push_back(std::stoi(numberInBraces));
-                    parentVector[i] = std::regex_replace(parentVector[i], patternParentOID, "");
-                }
-                else
-                {
-                    vectorOID.push_back(0);
-                }
-            }
-            
-            for (std::vector<std::string>::size_type i = parentVector.size() - 1; i != (std::vector<std::string>::size_type) - 1; i--)
-            {
-                if (parentVector[i] == "iso")
-                {
-                    m_pNode = m_pTree -> m_rootOfTheTree;
-                    vectorOID.erase(vectorOID.begin());
-                }
-                else
-                {
-                    //Add parent nodes
-                    m_pNode = m_pTree -> addNode(vectorOID.front(), parentVector[i], nullptr, "", Visibility::NONE, EncodingType::NONE, AccessType::NONE, StatusType::NONE, m_pNode);
-                    vectorOID.erase(vectorOID.begin());
-                    
-                }    
-            }
-            //Add node
-            m_pNode = m_pTree -> addNode(OID, nodeName, nullptr, "", Visibility::NONE, EncodingType::NONE, AccessType::NONE, StatusType::NONE, m_pNode);
+            addParentNodes(parentVector, vectorOID);
+            m_pNode = addNodeFromImport(nodeName, OID, parentVector); 
         }
         else
         {
-            for (std::list<Node>::iterator it = m_pTree -> m_nodeList.begin(); it != m_pTree -> m_nodeList.end(); it++)
-            {
-                if ((*it).m_name == parentVector.front())
-                {
-                    m_pNode = &(*it);
-                    break;
-                }
-            }
-            m_pNode = m_pTree -> addNode(OID, nodeName, nullptr, "", Visibility::NONE, EncodingType::NONE, AccessType::NONE, StatusType::NONE, m_pNode);
+            m_pNode = addNodeFromImport(nodeName, OID, parentVector);
         }
-        parentVector.clear();    
+        fileContent = std::regex_replace(fileContent, patternOIDLine, "", std::regex_constants::format_first_only);
+        parentVector.clear();
+        vectorOID.clear();
     }   
 }
 
@@ -134,3 +68,92 @@ void Parser::parseMIBFile(std::string fileContent)
 }
 
 
+std::string Parser::parseLineForNodeNameInImport(std::string & nodeLine, std::string & nodeName)
+{
+    std::regex patternNodeName("(\\w*)\\s*OBJECT IDENTIFIER");
+    std::regex patternCutNodeName("");
+    std::smatch match;
+    if (std::regex_search(nodeLine, match, patternNodeName))
+    {
+        nodeName = match.str(1);
+        patternCutNodeName.assign(nodeName);
+        nodeLine = std::regex_replace(nodeLine, patternCutNodeName, "", std::regex_constants::format_first_only);
+        return nodeName;
+    }
+    return "";
+}
+
+unsigned int Parser::parseLineForOIDInImport(std::string & nodeLine, unsigned int & OID)
+{
+    std::regex patternOID("(\\d)\\s\\}");
+    std::smatch match;
+    if (std::regex_search(nodeLine, match, patternOID))
+    {
+        OID = std::stoi(match.str(1));
+        return OID;
+    }
+    return 0;
+}
+
+void Parser::parseLineForParentNamesInImport(std::string & nodeLine, std::vector<std::string> & parentVector, std::vector<unsigned int> & vectorOID)
+{
+    std::regex patternParentNames("\\s+?([^ \\{]\\S*)\\s+?\\d\\s*\\}");
+    std::regex patternParentOID("\\((.)\\)");
+    std::regex patternCutBraceLeft("\\(");
+    std::regex patternCutBraceRight("\\)");
+    std::smatch match;
+    std::string matchString;
+    std::string cutString;
+    
+    while (std::regex_search(nodeLine, match, patternParentNames))
+    {
+        matchString = match.str(1);
+        cutString = matchString;
+        if (std::regex_search(matchString, match, patternParentOID))
+        {
+            vectorOID.push_back(std::stoi(match.str(1)));
+            matchString = std::regex_replace(matchString, patternParentOID, "", std::regex_constants::format_first_only);
+            parentVector.push_back(matchString);
+        }
+        else
+        {
+            vectorOID.push_back(0);
+            parentVector.push_back(matchString);
+        }
+        cutString = std::regex_replace(cutString, patternCutBraceLeft, "\\(", std::regex_constants::format_first_only);
+        cutString = std::regex_replace(cutString, patternCutBraceRight, "\\)", std::regex_constants::format_first_only);
+        std::regex patternCut(cutString);
+        nodeLine = std::regex_replace(nodeLine, patternCut, "", std::regex_constants::format_first_only);
+    } 
+}
+
+void Parser::addParentNodes(std::vector<std::string> & parentVector, std::vector<unsigned int> & vectorOID)
+{ 
+    std::regex patternParentOID("\\(.\\)");
+    std::smatch match;
+
+    for (std::vector<std::string>::size_type i = parentVector.size() - 1; i != (std::vector<std::string>::size_type) - 1; i--)
+    {
+        if (parentVector[i] == "iso")
+        {
+            m_pNode = m_pTree -> m_rootOfTheTree;
+        }
+        else
+        {
+            m_pNode = m_pTree -> addNode(vectorOID[i], parentVector[i], nullptr, "", Visibility::NONE, EncodingType::NONE, AccessType::NONE, StatusType::NONE, m_pNode);
+        }    
+    }
+}
+
+Node * Parser::addNodeFromImport(std::string & nodeName, unsigned int & OID, std::vector<std::string> & parentVector)
+{
+    for (std::list<Node>::iterator it = m_pTree -> m_nodeList.begin(); it != m_pTree -> m_nodeList.end(); it++)
+    {
+        if ((*it).m_name == parentVector.front())
+        {
+            m_pNode = &(*it);
+            break;
+        }
+    }
+    return m_pTree -> addNode(OID, nodeName, nullptr, "", Visibility::NONE, EncodingType::NONE, AccessType::NONE, StatusType::NONE, m_pNode);
+}
